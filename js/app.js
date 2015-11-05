032a5774aa918707c82cf7e89714b665373148d3
EL <eljhkrr@gmail.com>
2015-11-05 21:32:01 +0300

    Removed old controller and callback; added links for forks & stargazers

--- js/app.js
+++ js/app.js
@@ -22,28 +22,3 @@ app.controller("StarController", ["$scope", "$http", function($scope, $http){
 	});
 
 }]);
-
-// app.controller("StarController", function($scope, $http){
-
-// 	$http.jsonp('http://api.github.com/users/TheRakken/starred?callback=kallbuck')
-// 	.then(function successCallback(response) {
-// 		// this callback will be called asynchronously
-// 		// when the response is available
-// 		console.log("success callback");
-// 	console.log(response.data);
-// 	}, function errorCallback(response) {
-// 		// called asynchronously if an error occurs
-// 		// or server returns response with an error status.
-// 		console.log("error callback");
-// 		console.log(response.headers);
-// 	});
-
-// 	$scope.repos = repos;
-
-// });
-
-function kallbuck(json){
-	console.log("This is kallbuck");
-	console.log(json);
-	repos = json;
-}
