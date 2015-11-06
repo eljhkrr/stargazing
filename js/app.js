638aff046b42d233880fd0e244b248097f51bd41
EL <eljhkrr@gmail.com>
2015-11-06 07:28:42 +0300

    added http call for repo readme content

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 var app = angular.module("stargazing", []);
 var repos = 0;
+//var 
 
 app.controller("StarController", ["$scope", "$http", function($scope, $http){
 
 	$scope.data = null;
 	$scope.repository_list = [];
+	$scope.readme = "Select a repo";
 
 
 	$http({
@@ -20,5 +22,25 @@ app.controller("StarController", ["$scope", "$http", function($scope, $http){
 		$scope.data = response.data || "No response data";
 		$scope.repository_list = response.data || "No response data";
 	});
+
+	$scope.showReadme = function(name){
+		// console.log(name);
+		var readme_url = 'http://api.github.com/repos/'+name+'/readme?callback=JSON_CALLBACK';
+		// console.log(url);
+		$http({
+			method: 'JSONP',
+			url: readme_url
+		}).then(function(response){
+			//$scope.status = response.status;
+			$scope.rmdata = response.data;
+			//$scope.repository_list = response.data["data"];
+			$scope.readme = response.data["data"];
+			console.log(response.data);
+		},function(response){
+			//$scope.status = response.status;
+			$scope.rmdata = response.data || "No response data";
+			//$scope.repository_list = response.data || "No response data";
+		});
+	};
 
 }]);
