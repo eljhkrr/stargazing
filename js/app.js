631ef1092028fdb31ccfb57ab1bc4942e88aa412
EL <eljhkrr@gmail.com>
2015-11-28 19:09:52 +0300

    Getting all repos

--- js/app.js
+++ js/app.js
@@ -21,6 +21,8 @@ app.controller("StarController", ["$scope", "$http", "base64", "$sce", function(
 	$scope.repository_list = [];
 	$scope.readme = "Select a repo";
 	$scope.readme.markdown = "Select a repo to view details";
+	$scope.username = "therakken";
+	$scope.base_url = 'http://api.github.com/users/' + $scope.username + '/starred?callback=JSON_CALLBACK';
 
 	$scope.deliberatelyTrustDangerousSnippet = function() {
 	  return $sce.trustAsHtml($scope.readme.markdown);
@@ -29,12 +31,34 @@ app.controller("StarController", ["$scope", "$http", "base64", "$sce", function(
 
 	$http({
 		method: 'JSONP',
-		url: 'http://api.github.com/users/therakken/starred?callback=JSON_CALLBACK'
+		//url: 'http://api.github.com/users/' + $scope.username + '/starred?callback=JSON_CALLBACK&per_page=100'
+		url: $scope.base_url
 	}).then(function(response){
 		$scope.status = response.status;
 		$scope.data = response.data;
 		$scope.repository_list = response.data["data"];
-		console.log(response.data);
+		// console.log(response.data.meta);
+		// iterate through all pages
+		if ('Link' in response.data.meta){
+			console.log("property exists!");
+			console.log("pages: ");
+			
+			var url = response.data.meta.Link[1][0];
+			var ptn = /&page=/i;
+			console.log(url);
+			var pages = url.slice(url.search(ptn) + 6, url.length);
+			console.log("pages = " + pages);
+			if (isNaN(pages)){
+				// display some error message
+			} else {
+				// call method pass in no. of pages
+				$scope.getAllPages(url, pages);
+			}
+	
+		} else {
+			// There are no other pages
+		}
+
 	},function(response){
 		$scope.status = response.status;
 		$scope.data = response.data || "No response data";
@@ -63,5 +87,52 @@ app.controller("StarController", ["$scope", "$http", "base64", "$sce", function(
 			//$scope.repository_list = response.data || "No response data";
 		});
 	};
+
+	$scope.getAllPages = function(link, pages){
+		console.log("Link: " + link);
+		console.log("Pages: " + pages);
+		for (var i = 1; i < pages; i++){
+			//console.log("Fetching page: " + (i + 1));
+			var url = $scope.base_url + "&page=" + (i + 1);
+			console.log(url);
+			$http({
+				method: 'JSONP',
+				url: url
+			}).then(function(response){
+				//console.log("Repo list length=" + $scope.repository_list.length);
+				$scope.repository_list= $scope.repository_list.concat(response.data.data);
+				console.log($scope.repository_list);
+				//$scope.$apply();
+			}, function(response){
+				console.log("$http failed");
+			});
+		}
+
+	};
+
+	$scope.getAllPages1 = function(link, pages){
+		console.log("Link: " + link);
+		console.log("Pages: " + pages);
+		var i = 1;
+		var url = $scope.base_url + "&page=" + (i + 1);
+		console.log(url);
+		console.log("Repo list:");
+		console.log($scope.repository_list);
+		$http({
+			method: 'JSONP',
+			url: url
+		}).then(function(response){
+			//console.log("Repo list length=" + $scope.repository_list.length);
+			$scope.repository_list= $scope.repository_list.concat(response.data.data);
+			console.log("Response:");
+			console.log(response.data);
+			console.log($scope.repository_list);
+		}, function(response){
+			console.log("$http failed");
+		});
+		console.log("Repo list end=\n");
+		console.log($scope.repository_list);
+	};
+
 
 }]);
